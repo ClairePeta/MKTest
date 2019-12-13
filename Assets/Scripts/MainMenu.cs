bb1e2e9fff95f4efa076a871650bb556a5b1c983
ClairePeta <s3663435@student.rmit.edu.au>
2019-12-13 19:02:06 +1100

    Main Menu Setup
    Set up canvas with start menu(buttons: play game, settings and guit), on play button click how to play information is displayed; this canvas has 2 buttons play game(loads game scene) or back (start menu scene) and the settings option has a button to toggle the background music

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject Menu;
+    public GameObject PlayMenu;
+    public GameObject SettingsMenu;
+
+    public Button MusicButton;
+    public AudioSource backgroundMusic;
+
+    private void Awake()
+    {
+        Menu.SetActive(true);
+        SettingsMenu.SetActive(false);
+        PlayMenu.SetActive(false);
+    }
+
+    public void OnHowToPlayClick()
+    {
+        Menu.SetActive(false);
+        PlayMenu.SetActive(true);
+    }
+
+    public void OnSettingsClick()
+    {
+        Menu.SetActive(false);
+        SettingsMenu.SetActive(true);
+    }
+
+    public void OnBackClick()
+    {
+        Menu.SetActive(true);
+        SettingsMenu.SetActive(false);
+        PlayMenu.SetActive(false);
+    }
+
+    public void OnContinueClick()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+    }
+
+    public void OnMusicVolumeClick()
+    {
+        if (Globals.musicVolume == true)
+        {
+            backgroundMusic.Stop();
+            MusicButton.GetComponentInChildren<Text>().text = "Music Volume: OFF";
+            Globals.musicVolume = false;
+        }
+        else
+        {
+            backgroundMusic.Play();
+            MusicButton.GetComponentInChildren<Text>().text = "Music Volume: ON";
+            Globals.musicVolume = true;
+        }
+    }
+
+    public void OnQuitClick()
+    {
+        Application.Quit();
+    }
+}
