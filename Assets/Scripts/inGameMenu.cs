8d53240767950fdb9e27c65b92eb7dc54b77ab00
ClairePeta <s3663435@student.rmit.edu.au>
2019-12-13 21:39:19 +1100

    User input canvas
    Create the game canvas for user input, includes two lower buttons for jumping and ducking, there is a icon and score count for the number of object (carrots at this time) collected, and a in game menu button which will pause the game, from here the player can quit or continue. Basic coding done

---
+++ Assets/Scripts/inGameMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inGameMenu : MonoBehaviour
+{
+    public GameObject menu;
+
+    public void onbuttonClick()
+    {
+        menu.SetActive(true);
+        //pause the speed/running here
+    }
+
+    public void OnResumeClick()
+    {
+        menu.SetActive(false);
+        //resume speed/running here
+    }
+
+    public void OnMainMenuClick()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Main Menu");
+    }
+    public void OnQuitClick()
+    {
+        Application.Quit();
+    }
+}
