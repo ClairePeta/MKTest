4a60db78a1368a322b99fc92a3479836d1e59d37
ClairePeta <s3663435@student.rmit.edu.au>
2019-12-15 20:16:53 +1100

    Final Changes before final build

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -1,8 +1,6 @@
 ﻿public static class Globals
 {
     public static bool musicVolume = true;
-    public static string[] names = { "a", "b", "c", "d", "e" };
-    public static int[] score = { 5, 4, 3, 2, 1 };
     public static int lives;
     public static int gameScore;
     public static bool paused = false;
--- Assets/Scripts/HighScores.cs
+++ Assets/Scripts/HighScores.cs
@@ -4,6 +4,9 @@ using UnityEngine.SceneManagement;
 public class HighScores : MonoBehaviour
 {
     //change to playerprefs
+    string[] scoreNames = new string[5];
+    int[] scoreScores = new int[5];
+
     public TMPro.TextMeshProUGUI names;
     public TMPro.TextMeshProUGUI scores;
     int currentScore;
@@ -12,42 +15,77 @@ public class HighScores : MonoBehaviour
 
     void Start()
     {
+        /*PlayerPrefs.SetString("Name1", "a");
+        PlayerPrefs.SetString("Name2", "b");
+        PlayerPrefs.SetString("Name3", "c");
+        PlayerPrefs.SetString("Name4", "d");
+        PlayerPrefs.SetString("Name5", "e");
+
+        PlayerPrefs.SetInt("Score1", 5);
+        PlayerPrefs.SetInt("Score2", 4);
+        PlayerPrefs.SetInt("Score3", 3);
+        PlayerPrefs.SetInt("Score4", 2);
+        PlayerPrefs.SetInt("Score5", 1);
+        */
+        
+        for (int i = 0; i < 5; i++)
+        {
+            string scoreboardName = "Name" + (i+1).ToString();
+            string scoreboardScore = "Score" + (i+1).ToString();
+            scoreNames[i] = PlayerPrefs.GetString(scoreboardName);
+            scoreScores[i] = PlayerPrefs.GetInt(scoreboardScore);
+        }
+
         //gets the players score and compares it against the current scoreboard scores
         currentScore = Globals.gameScore;
-        foreach (int score in Globals.score)
+        foreach (int score in scoreScores)
         {
             if (currentScore <= score)
             {
-                scorePosition = position;
+                scorePosition = position+1;
             }
             position++;
         }
+        Debug.Log("scorePosition1" + scorePosition);
         scorePosition++;
+        Debug.Log("scorePosition2" + scorePosition);
         //lowers all the names so that the current players name is in all caps
-        foreach (string name in Globals.names)
+        foreach (string name in scoreNames)
         {
             name.ToLower();
         }
         //moves around the scoreboard if needed
         if (scorePosition < 4)
         {
-            for (int i = (Globals.names.Length - 1); i >= scorePosition; i--)
+            for (int i = (scoreNames.Length - 1); i >= scorePosition; i--)
             {
-                Globals.names[i] = Globals.names[i-1];
-                Globals.score[i] = Globals.score[i-1];
+                scoreNames[i] = scoreNames[i-1];
+                scoreScores[i] = scoreScores[i-1];
             }
+
             scorePosition--;
-            Globals.names[scorePosition] = Globals.playerName.ToUpper();
-            Globals.score[scorePosition] = currentScore;
+            Debug.Log("scorePosition3" + scorePosition);
+            scoreNames[scorePosition] = Globals.playerName.ToUpper();
+            scoreScores[scorePosition] = currentScore;
         }
         if(scorePosition == 4)
         {
-            Globals.names[scorePosition] = Globals.playerName.ToUpper();
-            Globals.score[scorePosition] = currentScore;
+            scoreNames[scorePosition] = Globals.playerName.ToUpper();
+            scoreScores[scorePosition] = currentScore;
         }
-        //sets the names and score to be displayed on teh canvas
-        names.text = (Globals.names[0] + "\n" + Globals.names[1] + "\n" + Globals.names[2] + "\n" + Globals.names[3] + "\n" + Globals.names[4]);
-        scores.text = (Globals.score[0] + "\n" + Globals.score[1] + "\n" + Globals.score[2] + "\n" + Globals.score[3] + "\n" + Globals.score[4]);
+        Debug.Log("scorePosition4" + scorePosition);
+        for (int i = 0; i < 5; i++)
+        {
+            string scoreboardName = "Name" + (i + 1).ToString();
+            string scoreboardScore = "Score" + (i + 1).ToString();
+
+            PlayerPrefs.SetString(scoreboardName, scoreNames[i].ToLower());
+            PlayerPrefs.SetInt(scoreboardScore, scoreScores[i]);
+        }
+
+        //sets the names and score to be displayed on the canvas
+        names.text = (scoreNames[0] + "\n" + scoreNames[1] + "\n" + scoreNames[2] + "\n" + scoreNames[3] + "\n" + scoreNames[4]);
+        scores.text = (scoreScores[0] + "\n" + scoreScores[1] + "\n" + scoreScores[2] + "\n" + scoreScores[3] + "\n" + scoreScores[4]);
     }
 
     public void OnMainMenuClick()
